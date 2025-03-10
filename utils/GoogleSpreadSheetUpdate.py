ccd37d2703c52e41909516ae8aeeab9d16dd99d1
Buddhika Madusanka <buddhikadevelopment@gmail.com>
2025-03-10 09:56:49 +0530

    Configure the function for working with google api and update the spreadsheet

---
+++ utils/GoogleSpreadSheetUpdate.py
@@ -0,0 +1,60 @@
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = ["https://www.googleapis.com/auth/spreadsheets"]
+SHEET_ID = "13mCs-Vwv4EbT2ANuykpIL2_hr1ZghabkWSEmEkwc0cw"
+
+CREDENTIALS = Credentials.from_service_account_file("credentials.json", scopes = SCOPE)
+CLIENT = gspread.authorize(CREDENTIALS)
+WORKBOOK = CLIENT.open_by_key(SHEET_ID)
+
+worksheet_list = map(lambda x: x.title, WORKBOOK.worksheets())
+worksheet_name = "applicant_details"
+ 
+# check worksheet already exists or not
+def check_worksheet_existance() :
+    if worksheet_name in worksheet_list :
+        # access the sheet need to update
+        sheet = WORKBOOK.worksheet(worksheet_name)
+    else :
+        # create sheet for updates
+        sheet = WORKBOOK.add_worksheet(worksheet_name)
+
+    return sheet
+
+
+# update google sheet with the applicant details
+def update_google_spread_sheet(name, mail, contact, education, qualification, experience):
+    sheet = check_worksheet_existance()
+
+    all_values = sheet.get_all_values()
+    row_count = len(all_values)
+    next_row = int(row_count) + 1
+
+    try :
+        sheet.update_cell( next_row,1, name )
+        sheet.update_cell( next_row,2, mail )
+        sheet.update_cell( next_row,3, contact)
+        sheet.update_cell( next_row,4, education )
+        sheet.update_cell( next_row,5, qualification )
+        sheet.update_cell( next_row,6, experience )
+
+        return True
+    except: 
+        print("Something went wrong")
+        return False
+
+
+write_applicant_details_status = update_google_spread_sheet(
+    "Chandima Sandamali",
+    "chandima@gmail.com",
+    "0702276557",
+    "Undergraduate in Ruhuna university",
+    "Working in larg scale project and working close with devops",
+    "Large scale ERP system"
+)
+
+if write_applicant_details_status :
+    print("Successfully")
+else :
+    print("Something went wrong")
